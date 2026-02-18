bbd957d60cfd1c45776a61566e88767a46bff87d
omejiariv <145706652+omejiariv@users.noreply.github.com>
2026-02-17 21:18:25 -0500

    Update 09_👑_Panel_Administracion.py

--- pages/09_👑_Panel_Administracion.py
+++ pages/09_👑_Panel_Administracion.py
@@ -1109,7 +1109,7 @@ with tabs[14]:  # (Asegúrate de que esta variable coincida con tu st.tabs)
         nombre_plantilla = "plantilla_edades_piramide.csv"
         desc_ayuda = "Debe contener el año, la edad simple (0, 1, 2...) y la cantidad de Hombres, Mujeres y el Total."
 
-   col_down, col_up = st.columns([1, 2])
+    col_down, col_up = st.columns([1, 2])
     
     # 3. Opción de Descargar Plantilla Vacía
     with col_down:
@@ -1164,6 +1164,7 @@ with tabs[14]:  # (Asegúrate de que esta variable coincida con tu st.tabs)
                         
             except Exception as e:
                 st.error(f"Ocurrió un error al procesar el archivo: {e}")
+
 
 
 
