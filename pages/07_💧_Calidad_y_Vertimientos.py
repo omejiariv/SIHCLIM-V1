bac91c300730468e5aed5a74f6df7782bc3d9272
omejiariv <145706652+omejiariv@users.noreply.github.com>
2026-03-18 20:18:28 -0500

    Update 07_💧_Calidad_y_Vertimientos.py

--- pages/07_💧_Calidad_y_Vertimientos.py
+++ pages/07_💧_Calidad_y_Vertimientos.py
@@ -393,10 +393,6 @@ with st.spinner(f"Cruzando datos espacialmente con {nombre_seleccion}..."):
     # 2. CONCESIONES
     # ---------------------------------------------------------
     if not df_concesiones.empty:
-        # ---------------------------------------------------------
-    # 2. CONCESIONES
-    # ---------------------------------------------------------
-    if not df_concesiones.empty:
         # ⚠️ CORRECCIÓN: Nombres exactos mapeados en la función cargar_concesiones
         cols_c = [c for c in df_concesiones.columns if c in ['caudal_lps', 'tipo_agua', 'Sector_Sihcli', 'coordenada_x', 'coordenada_y', 'uso_detalle', 'estado']]
         df_c_light = df_concesiones[cols_c].copy()
